94a8393b3d47fb1553df8592a2ca342accdb1449
pedamalluv <vikramatfreelance@gmail.com>
2025-03-19 00:08:55 +0530

    Auto-generated commit

--- src/app/directives/disable-element.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DisableElementPipe } from './disable-element.pipe';
-
-describe('DisableElementPipe', () => {
-  it('create an instance', () => {
-    const pipe = new DisableElementPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
